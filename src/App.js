58668322e324456feada9cb17b4a44c9fce08368
John Kim <yubbykim@gmail.com>
2024-01-21 00:24:01 -0800

    buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+const loginInfo = {
+  "name": "yubby kim",
+  "email": "john-0904@hotmail.com",
+  "password": "yubbykim",
+  "phone_number": 6824452890,
+  "location": "coquitlam",
+  "bio": "i like solar panels"
+}
+
+const handleClick = () => {
+  console.log("sending request")
+  const request = new Request("http://localhost:3000/profiles", {
+    method: "post",
+    body: JSON.stringify(loginInfo),
+    headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+    }
+});
+
+fetch(request)
+    .then(res => {
+        if (res.status === 201) {
+          console.log("successful!")
+            return res.json()
+
+
+        }
+    })
+
+    .catch(error => {
+        console.log(error);
+    });
+}
 
 function App() {
   return (
@@ -9,6 +46,8 @@ function App() {
         <p>
          Hello world.
         </p>
+        <TextField id="outlined-basic" label="Name" variant="outlined" />
+        <Button variant="contained" onClick={handleClick}>Register!</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
